756ef10f02f3f2070fed73e3d6d9613597fe0099
Bilel MEMDOUH <bilel.memdouh@savoirfairelinux.com>
2017-08-04 16:39:44 -0400

    basic configuration for rest api

--- src/main/java/com/bilelovic/warsh/kiraate/repository/KiraaRepository.java
+++ src/main/java/com/bilelovic/warsh/kiraate/repository/KiraaRepository.java
@@ -1,11 +1,13 @@
 package com.bilelovic.warsh.kiraate.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import com.bilelovic.warsh.kiraate.domain.Kiraa;
 
 @Repository
+//@RestResource(exported=false)
 public interface KiraaRepository extends MongoRepository<Kiraa, String> {
 
 }
